3b1298e84ac930f40ba2565afb9815aaa7c95653
EmilEinarsen <47479113+EmilEinarsen@users.noreply.github.com>
2020-10-27 22:32:29 +0100

    cleaned the code

--- index.js
+++ index.js
@@ -8,131 +8,142 @@ const data = {
 	isDrawing: false,
 	start: {},
 	lines: [],
+	mouseCoordinates: e => ({
+		x: e.clientX - data.bounds.left,
+		y: e.clientY - data.bounds.top
+	})
 }
 
-window.onload = function() {
+window.addEventListener('load', () => {
+
 	canvas = document.querySelector("canvas")
 	canvas.width = 500
 	canvas.height = 500
-	canvas.onmousedown = onmousedown
-	canvas.onmouseup = onmouseup
-	canvas.onmousemove = onmousemove
-	
-	data.bounds = canvas.getBoundingClientRect()
-	console.log(data.bounds)
-	ctx = canvas.getContext("2d")
-	data.isLoaded = true
-	draw()
-}
 
-function draw(e) {
+	canvas.addEventListener('mousedown', e => {
+		if (!data.isLoaded || data.isDrawing) return
+		data.start = data.mouseCoordinates(e)
+		data.isDrawing = true
+		newLayer(e)
+	})
+
+	canvas.addEventListener('mousemove', e => data.isLoaded && data.isDrawing && newLayer(e) )
+
+	canvas.addEventListener('mouseup', e => {
+		if (!(data.isLoaded && data.isDrawing)) return
+
+		data.lines.push({ start: data.start, length: getLength(e) })
+
+		data.isDrawing = false
+		newLayer(e)
+	})
+
+	data.bounds = canvas.getBoundingClientRect()
+
+	ctx = canvas.getContext("2d")
+
+	data.isLoaded = true
+	newLayer()
+	
+})
+
+
+
+function newLayer(e) {
+	const { lines, isDrawing, start } = data
+
+	// Hiddes oldlayer
 	ctx.fillStyle = "#eee"
 	ctx.fillRect(0,0,canvas.width,canvas.height)
 	
-	ctx.strokeStyle = "black"
-	ctx.lineWidth = 2
-	data.lines.forEach(line => drawLine(line))
+	// Redraws saved lines
 	
+	lines.forEach(line => draw(line))
 	
-	if (data.isDrawing) {
-		ctx.strokeStyle = "darkred"
-		ctx.lineWidth = 3
-		drawLine({
-			start: data.start, 
-			length: getLength(e),
-		})
-	}
-}
+	// draws active line
+	if (!isDrawing) return
+	draw({ start: start, length: getLength(e) }, true)
 
-function onmousedown(e) {
-	if (!data.isLoaded) return 
-	if (!data.isDrawing) {
-		data.start = getMouseCoordinates(e)
-		//console.log(getMouseCoordinates(e))
+	function draw({start, length}, active) {
+		if(active) {
+			ctx.strokeStyle = "darkred"
+			ctx.lineWidth = 3
+		} else {
+			ctx.strokeStyle = "black"
+			ctx.lineWidth = 2
+		}
 		
-		data.isDrawing = true
-	}
-	draw(e)
-}
+		const end = {
+			x: start.x + length.x,
+			y: start.y + length.y,
+		}
 
-function onmousemove(e) {
-	if (!(data.isLoaded && data.isDrawing)) return
-	//console.log(data.lengths)
-	draw(e)
-}
-
-function onmouseup(e) {
-	if (!(data.isLoaded && data.isDrawing)) return
-
-	data.lines.push({ start: data.start, length: getLength(e) })
-
-	data.isDrawing = false
-	draw(e)
-}
-
-function getMouseCoordinates(e) {
-	return {
-		x: e.clientX - data.bounds.left,
-		y: e.clientY - data.bounds.top
+		ctx.beginPath()
+		ctx.moveTo(start.x, start.y)
+		ctx.lineTo(end.x, end.y)
+		ctx.stroke()
 	}
 }
 
 function getLength(e) {
 	const { snapDegree } = preset
+	const { sqrt, pow, atan, PI, round, cos, sin } = Math
+	const { mouseCoordinates, start } = data
 
-	const { x, y, d } = getLengths()
-	// flips the length inaccordance with x-axis, results in 360 degrees instead of 180
-	const length = (x < 0 ? -1 : 1) * d 
+	let { x, y, h } = getLengths()
+
+	// flips the hypotenuse inaccordance with x-axis, results in 360 degrees instead of 180
+	h = (x < 0 ? -1 : 1) * h 
 	
 	/**
 	 * Length is sufficent to draw the line. 
-	 * However, since we want the angle to be a mutiple of snapDegree,
-	 * it has to be round of to the nerest mutiple 
+	 * However, since the angle needs to be a mutiple of snapDegree,
+	 * it has to be round off to the nerest mutiple 
 	 * and thereafter used to calculate the true length
 	 */
 	const angle = getAngle()
-	return { 
-		x: length * Math.cos(angle), 
-		y: length * Math.sin(angle),
-	}
+	x = h * cos(angle)
+	y = h * sin(angle)
+	h = pythagorean(x,y)
+
+	/**
+	 * x and y are used to draw the line. 
+	 * However hypotenuse might be better used in calculations
+	 */
+	return { x, y, h }
 
 	function getLengths() {
 		// end point
-		const end = getMouseCoordinates(e)
+		const end = mouseCoordinates(e)
 
 		// length of x-axis
-		const x = - data.start.x + end.x
+		const x = end.x - start.x
 
 		// length of y-axis
-		const y = - data.start.y + end.y
+		const y = end.y - start.y
 
-		// length is the diagonal that x and y results in
-		let d = Math.sqrt( (Math.pow(x,2) + Math.pow(y,2) ))
+		// the hypotenuse that x and y results in
+		let h = pythagorean(x,y)
 
-		return { x, y, d }
+		return { x, y, h }
+	}
+
+	function pythagorean( x, y ) {
+		return sqrt(( pow(x,2) + pow(y,2) ))
 	}
 
 	function getAngle() {
-		const arctan = Math.atan( y / x )
+		const arctan = atan( y / x )
 		
 		// converts arctan to degrees
-		let degrees = arctan * 180 / Math.PI
+		let degrees = arctan * 180 / PI
 		
 		// rounds of to the nerest multiple of snapDegree 
-		degrees = snapDegree * Math.round(degrees/snapDegree)
+		degrees = snapDegree * round(degrees/snapDegree)
 		
 		// converts degrees to arctan 
-		return degrees * Math.PI / 180
-	}
-}
+		const angle = degrees * PI / 180
 
-function drawLine({start, length}) {
-	const end = {
-		x: start.x + length.x,
-		y: start.y + length.y,
+		return angle
 	}
-	ctx.beginPath()
-	ctx.moveTo(start.x, start.y)
-	ctx.lineTo(end.x, end.y)
-	ctx.stroke()
 }
