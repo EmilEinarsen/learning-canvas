c722d0d25ea2e7f179043da6e796f49f26da3c63
EmilEinarsen <47479113+EmilEinarsen@users.noreply.github.com>
2020-10-29 14:26:21 +0100

    Updated the code to use coordinates instead of lines. Addtionally, cleaned the code and restructured it

--- index.js
+++ index.js
@@ -2,129 +2,178 @@ let canvas, ctx
 const preset = {
 	snapDegree: 15,
 	drawRadius: 12.5,
-	lineWidth: 5
+	lineWidth: 5,
+	activeLineWidthMultiplier: 1.5,
 }
 const data = {
 	bounds: null,
 	isLoaded: false,
 	isDrawing: false,
 	start: {},
-	end: {},
 	coordinates: [],
-	mouseCoordinates: e => ({
+	mouseCoordinate: e => ({
 		x: e.clientX - data.bounds.left,
 		y: e.clientY - data.bounds.top
 	}),
-	endCoordinates: (start, length) => ({
+	endCoordinate: (start, length) => ({
 		x: start.x + length.x,
 		y: start.y + length.y,
 	}),
-	whichEdge: true
+	lineEdges: [],
+
+	/**
+	 * ENUM for edge definition
+	 * left and right symbolises 
+	 * the position in lineEdges
+	 * not on screen
+	 */
+	EDGES: {
+		LEFT: 0,
+		RIGHT: 1,
+		NONE: NaN,
+	},
+
+	/** tracks active edge */
+	edge: NaN,
 }
 
-window.addEventListener('load', () => {
 
+
+/** 
+ * ! Process 
+*/
+window.onload = () => {
 	canvas = document.querySelector("canvas")
-	canvas.width = 500
-	canvas.height = 500
-
-	canvas.addEventListener('mousedown', e => {
-		if (!data.isLoaded || data.isDrawing) return
-
-		// Controlls drawing 
-		shouldDraw(e)
-
-		// if(data.isDrawing) newLayer(e)
-	})
-
-	canvas.addEventListener('mousemove', e => {
-		if(!(data.isLoaded && data.isDrawing)) return
-
-		data.end = data.endCoordinates(data.start, getLength(e))
-
-		newLayer(e)
-	} )
-
-	canvas.addEventListener('mouseup', e => {
-		if (!(data.isLoaded && data.isDrawing)) return
-
-		if(data.whichEdge) data.coordinates.push( data.end )
-		else if(!data.whichEdge) data.coordinates.unshift( data.end )
-
-		data.isDrawing = false
-		data.index = undefined
-		newLayer(e)
-	})
-
+	canvas.width = innerWidth
+	canvas.height = innerHeight
 	data.bounds = canvas.getBoundingClientRect()
-
 	ctx = canvas.getContext("2d")
+
+	canvas.onmousedown = mouseDown
+	canvas.onmousemove = mouseMove
+	canvas.onmouseup = mouseUp
 
 	data.isLoaded = true
 	newLayer()
+}
+mouseDown = e => {
+	if (!data.isLoaded || data.isDrawing) return
 
-})
+	drawProcess()
 
-function shouldDraw(e) {
-	const { drawRadius } = preset
-	const coordinates = data.mouseCoordinates(e)
 
-	/**
-	 * Returns true if it should draw.
-	 * Otherwise doesn't return, resulting in a undefined (falsy) return
-	 */
+	function drawProcess() {
+		requestNewLine(e)
 
-	// exception for the initial/first line
-	if(!data.coordinates.length) {
-		data.coordinates.push(coordinates)
-		data.start = coordinates
-		data.isDrawing = true
+		if(data.isDrawing) newLayer(e)
 	}
-	// if starting point is within drawRadius of a edge, then define it's index as index
-	else [ 
-		data.coordinates[0], 
-		data.coordinates[data.coordinates.length-1] 
-	].forEach((edge, index) => {
-		const distanceX = Math.abs(coordinates.x - edge.x)
-		const distanceY = Math.abs(coordinates.y - edge.y)
-		if(distanceX < drawRadius && distanceY < drawRadius) {
-			data.isDrawing = true
-			data.whichEdge = index
-			data.start = data.coordinates[index ? data.coordinates.length-1 : 0]
+}
+mouseMove = e => {
+	if(!data.isLoaded) return
+
+	data.isDrawing && drawProcess()
+
+
+	function drawProcess() {
+		const coordinate = data.endCoordinate(data.start, getLength(e))
+	
+		
+		if(data.lineEdges.length !== 1) data.lineEdges[data.edge] = coordinate
+
+		/** exception for the initial/first line */
+		else data.lineEdges[data.EDGES.RIGHT] = coordinate
+
+		newLayer(e)
+	}
+}
+mouseUp = e => {
+	if (!data.isLoaded) return
+
+	data.isDrawing && drawProcess()
+
+	
+	function drawProcess() {
+		const coordinate = data.endCoordinate(data.start, getLength(e))
+		data.lineEdges[data.edge] = coordinate
+		
+		/** pushes or unshifts coordinate onto 
+		 * data.coordinates depending on which edge 
+		 */
+		;( 
+			array => data.edge === data.EDGES.RIGHT ? array.push( coordinate ) : array.unshift( coordinate )
+		)(data.coordinates)
+
+		reset()
+		newLayer(e)
+
+		function reset() {
+			data.start = {}
+			data.edge = data.EDGES.NONE
+			data.isDrawing = false
+			data.index = undefined
 		}
+	}
+}
+/** 
+ * ! End Process 
+*/
+
+
+
+/** 
+ * ! Delegations 
+*/
+function requestNewLine(e) {
+	const { drawRadius } = preset
+	const coordinate = data.mouseCoordinate(e)
+
+	/** exception for the initial/first line */
+	if(!data.lineEdges.length) {
+		data.edge = data.EDGES.LEFT
+		data.lineEdges[data.EDGES.LEFT] = coordinate
+		data.coordinates.push(coordinate)
+	}
+	
+	/** if the requested start coordinate is within drawRadius 
+	 * of a edge coordinate, then define it as edge
+	 */
+	else data.lineEdges.forEach((edge, index) => {
+		const distanceX = Math.abs(coordinate.x - edge.x)
+		const distanceY = Math.abs(coordinate.y - edge.y)
+
+		if(distanceX < drawRadius && distanceY < drawRadius) 
+			if(Object.values(data.EDGES).includes(index))
+				data.edge = index
 	})
+
+	/** Allows drawing if the edge is allowed. (a perhaps not neccessary safe guard) */
+	if([data.EDGES.LEFT, data.EDGES.RIGHT].includes(data.edge)) 
+		data.isDrawing = true
+
+	data.start = data.lineEdges[data.edge]
 }
 
 function newLayer(e) {
-	const { coordinates, isDrawing, start, end, whichEdge } = data
-	const { drawRadius, lineWidth } = preset
-
 	// Hiddes oldlayer
 	ctx.fillStyle = "#eee"
 	ctx.fillRect(0,0,canvas.width,canvas.height)
 	
 	// redraws saved lines
-	if(coordinates.length > 1) drawStoredLines()
+	for(pointer = 1; pointer < data.coordinates.length; pointer++) drawLine({
+		start: data.coordinates[pointer-1],
+		end: data.coordinates[pointer],
+	})
 
-	// draws circles at the edges
-	whichEdge && drawCircle({ start: coordinates[0], radius: drawRadius })
-	!whichEdge && drawCircle({ start: coordinates[coordinates.length-1], radius: drawRadius })
-	drawCircle({ start: end, radius: drawRadius })
+	// draws circles at the edges, to mark where you can draw
+	data.lineEdges.forEach((coordinate, i) => drawCircle(
+		{ coordinate, radius: preset.drawRadius }, 
+		i === data.edge,
+	))
 	
 	// draws active line
-	if (!isDrawing) return
-	drawLine({ start, end }, true)
-	
+	if (!data.isDrawing) return
+	drawLine({ start: data.start, end: data.lineEdges[data.edge] }, true)
 
-	function drawStoredLines() {
-		let pointer = 1
-		while(pointer < coordinates.length){
-			drawLine({
-				start: coordinates[pointer-1], 
-				end: coordinates[pointer]
-			})
-			pointer++
-	}}
 
 	function drawLine({start, end}, active) {
 		lineStyle(active)
@@ -134,7 +183,7 @@ function newLayer(e) {
 		ctx.stroke()
 	}
 
-	function drawCircle({ start: { x, y }, radius }, active){
+	function drawCircle({ coordinate: { x, y }, radius }, active){
 		lineStyle(active)
 		ctx.beginPath()
 		ctx.arc(x, y, radius, 0, 2 * Math.PI)
@@ -144,19 +193,15 @@ function newLayer(e) {
 	function lineStyle(active) {
 		if(active) {
 			ctx.strokeStyle = "darkred"
-			ctx.lineWidth = lineWidth*1.5
+			ctx.lineWidth = preset.lineWidth*preset.activeLineWidthMultiplier
 		} else {
 			ctx.strokeStyle = "black"
-			ctx.lineWidth = lineWidth
+			ctx.lineWidth = preset.lineWidth
 		}
 	}
 }
 
 function getLength(e) {
-	const { snapDegree } = preset
-	const { sqrt, pow, atan, PI, round, cos, sin } = Math
-	const { mouseCoordinates, start } = data
-
 	let { x, y, h } = getLengths()
 
 	// flips the hypotenuse inaccordance with x-axis, results in 360 degrees instead of 180
@@ -169,9 +214,14 @@ function getLength(e) {
 	 * and thereafter used to calculate the true length
 	 */
 	const angle = getAngle()
-	x = h * cos(angle)
-	y = h * sin(angle)
+	x = h * Math.cos(angle)
+	y = h * Math.sin(angle)
 	h = pythagorean(x,y)
+	
+	// if x,y,h is anything falsy make it 0
+	!x && (x = 0)
+	!y && (y = 0)
+	!h && (h = 0)
 
 	/**
 	 * x and y are used to draw the line. 
@@ -181,13 +231,13 @@ function getLength(e) {
 
 	function getLengths() {
 		// end point
-		const end = mouseCoordinates(e)
+		const end = data.mouseCoordinate(e)
 
 		// length of x-axis
-		let x = end.x - start.x
+		let x = end.x - data.start.x
 
 		// length of y-axis
-		let y = end.y - start.y
+		let y = end.y - data.start.y
 
 		// the hypotenuse that x and y results in
 		let h = pythagorean(x,y)
@@ -196,20 +246,20 @@ function getLength(e) {
 	}
 
 	function pythagorean( x, y ) {
-		return sqrt(( pow(x,2) + pow(y,2) ))
+		return Math.sqrt(( Math.pow(x,2) + Math.pow(y,2) ))
 	}
 
 	function getAngle() {
-		const arctan = atan( y / x )
+		const arctan = Math.atan( y / x )
 		
 		// converts arctan to degrees
-		let degrees = arctan * 180 / PI
+		let degrees = arctan * 180 / Math.PI
 		
 		// rounds of to the nerest multiple of snapDegree 
-		degrees = snapDegree * round(degrees/snapDegree)
+		degrees = preset.snapDegree * Math.round(degrees/preset.snapDegree)
 		
 		// converts degrees to arctan 
-		const angle = degrees * PI / 180
+		const angle = degrees * Math.PI / 180
 
 		return angle
 	}
